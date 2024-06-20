74fd693c23ec317cfaa7cf79dc9a1a5175fc4c6d
Andreev Dmitry <demiusg@gmail.com>
2024-06-20 16:59:37 +0300

    Refactor: change reqirements.txt and start.bat

---
+++ Update_DB/start.bat
@@ -0,0 +1,1 @@
+"C:\Users\dmandree\OneDrive - Guess Inc\D Project\Test_script_development\Update_DB\venv\Scripts\python.exe" "C:\Users\dmandree\OneDrive - Guess Inc\D Project\Test_script_development\Update_DB\main.py"
